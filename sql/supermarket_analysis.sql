e7e003d012a21a37cb481d8dfece7bcaeae7fb5e
tapiwamapuranga <132844790+tapiwamapuranga@users.noreply.github.com>
2026-09-06 09:33:13 +0200

    Add SQL analysis file

---
+++ sql/supermarket_analysis.sql
@@ -0,0 +1,12 @@
+/*
+Project: Supermarket Sales Performance Analysis
+Author: Tapiwa Mapuranga
+Purpose: SQL analysis of supermarket sales data
+*/
+
+-- =====================================================
+-- 1. VIEW THE DATA
+-- =====================================================
+
+SELECT *
+FROM supermarket_sales;
